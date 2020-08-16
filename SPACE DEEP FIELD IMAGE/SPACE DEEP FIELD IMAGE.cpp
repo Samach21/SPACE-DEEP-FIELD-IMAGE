ff9a70f6db34ddaa026d6e5ec0254b4550636f8b
Samach21 <69465282+Samach21@users.noreply.github.com>
2020-08-16 23:50:48 +0700

    80%

--- SPACE DEEP FIELD IMAGE/SPACE DEEP FIELD IMAGE.cpp
+++ SPACE DEEP FIELD IMAGE/SPACE DEEP FIELD IMAGE.cpp
@@ -4,12 +4,16 @@
 #define MAX_SIZE (1000) 
 
 int x[MAX_SIZE][MAX_SIZE];
-int M_values, N_values, R_values[11], D_values[11], i, j;
-float Dis, D_float, XY_float, R_float;
-char status_[15];
+int M_values, N_values, i, j;
+int planet = 0, fixed_star = 0, comet = 0;
 
 int calculate();
 int process();
+int Check_Step1(int, int);
+int Check_Step2_1(int, int);
+int Check_Step2_2(int, int);
+int Check_Step3(int, int);
+int Check_Step4(int, int);
 
 int main()
 {
@@ -31,29 +35,21 @@ int main()
         return 1;
     }
 
-    for (i = 1; i <= N_values; i++) 
+    for (i = 1; i <= M_values; i++) 
     {
-        for (j = 1; j <= M_values; j++) {
+        for (j = 1; j <= N_values; j++) {
             if (scanf_s("%d", &x[i][j]) != 1) {
                 printf("ERROR \n");
                 return 1;
             }
-            else if (x[i][j] < -10000000 || x[i][j] > 10000000) {
-                printf("ERROR \n-10000000 <= x%d <= 10000000\n", j);
-                return 1;
-            }
-            if (scanf_s("%d", &y[j]) != 1) {
+            else if (x[i][j] < 0 || x[i][j] > 1) {
                 printf("ERROR \n");
                 return 1;
             }
-            else if (y[j] < -10000000 || y[j] > 10000000) {
-                printf("ERROR \n-10000000 <= y%d <= 10000000\n", j);
-                return 1;
-            }
         }
-        calculate();
     }
 
+    calculate();
     process();
 
     return 0;
@@ -61,37 +57,155 @@ int main()
 
 int process()
 {
-    for (int m = 1; m <= W_values; m++)
-    {
-        printf("\n%c", status_[m]);
-    }
-    return 0;
+    
 }
 
 int calculate()
 {
-    for (long int k = 1; k <= N_values[i]; k++)
+    for (i = 1; i <= M_values; i++)
     {
-        for (long int l = 1; l <= N_values[i] - 1; l++)
-        {
-            if (k != l)
+        for (j = 1; j <= N_values; j++) {
+            if (x[i][j] == 1)
             {
-                D_float = D_values[i];
-                R_float = R_values[i];
-                XY_float = (x[k] - x[l]) * (x[k] - x[l]) + (y[k] - y[l]) * (y[k] - y[l]);
-                Dis = sqrt(XY_float) - (2.0 * R_float);
-                if (Dis < D_float)
+                Check_Step1(i, j);
+            }
+        }
+    }
+    return 0;
+}
+
+int Check_Step1(int k, int l)
+{
+    if (x[k][l+1] == 1)
+    {
+        Check_Step2_1(k, l);
+    }
+    else
+    {
+        Check_Step2_2(k, l);
+    }
+    return 0;
+}
+
+int Check_Step2_1(int m, int n)
+{
+    if (x[m + 1][n] == 1)
+    {
+        planet += 1;
+        for (int o = m; o < (1000 - m); o++)
+        {
+            for (int p = n; p < (1000 - n); p++)
+            {
+                if (x[o][p] == 1)
                 {
-                    status_[i] = 'N';
-                    k = N_values[i] + 1;
-                    l = N_values[i];
+                    x[o][p] == 0;
                 }
                 else
                 {
-                    status_[i] = 'Y';
+                    if (x[o + 1][n] == 0)
+                    {
+                        o = 2000;
+                    }
+                    p = 2000;
                 }
             }
         }
     }
+    else
+    {
+        comet += 1;
+        int q = 0;
+        for (int o = m; o < (1000 - m); o++)
+        {
+            for (int p = n + q; p < (1000 - n); p++)
+            {
+                if (x[o][p] == 1)
+                {
+                    x[o][p] == 0;
+                }
+                else
+                {
+                    if (x[m + q + 1][n + q + 1] == 0)
+                    {
+                        o = 2000;
+                    }
+                    p = 2000;
+                }
+            }
+            q++;
+        }
+    }
+    return 0;
+}
+
+int Check_Step2_2(int m, int n)
+{
+    if (x[m + 1][n - 1] == 1)
+    {
+        Check_Step3(m, n);
+    }
+    else
+    {
+        comet += 1;
+        int q = 0;
+        for (int p = n; p < (1000 - n); p++)
+        {
+            for (int o = m + q; o < (1000 - m); o++)
+            {
+                if (x[o][p] == 1)
+                {
+                    x[o][p] == 0;
+                }
+                else
+                {
+                    if (x[m + q + 1][n + q + 1] == 0)
+                    {
+                        p = 2000;
+                    }
+                    o = 2000;
+                }
+            }
+            q++;
+        }
+    }
+    return 0;
+}
+
+int Check_Step3(int m, int n)
+{
+    if (x[m + 1][n + 1] == 1)
+    {
+        Check_Step4(m, n);
+    }
+    else
+    {
+        comet += 1;
+        int q = 0;
+        for (int p = n; p > 0; p--)
+        {
+            for (int o = m + q; o < (1000 - m); o++)
+            {
+                if (x[o][p] == 1)
+                {
+                    x[o][p] == 0;
+                }
+                else
+                {
+                    if (x[m + q + 1][n + q + 1] == 0)
+                    {
+                        p = 2000;
+                    }
+                    o = 2000;
+                }
+            }
+            q++;
+        }
+    }
+    return 0;
+}
+
+int Check_Step4()
+{
+
     return 0;
 }
